375b429b8cf32f5f8cd9e0f06a2eea823bb70b85
Slytherin⚕️ <142644436+Born-Slytherin@users.noreply.github.com>
2023-09-23 18:04:10 +0530

    Update 20_QueueInsert.cpp

--- 20_QueueInsert.cpp
+++ 20_QueueInsert.cpp
@@ -1,6 +1,5 @@
-/*Write a program to insert more than one element into a queue.*/
-
-#include<iostream>
+#include <iostream>
+#include <cctype>
 using namespace std;
 #define MAX 5
 
@@ -16,33 +15,47 @@ public:
     }
 
     void ReadQueue() {
-        if (rear < size - 1) { 
+        if (rear < size - 1) {
             for (; rear < size - 1; rear++) {
                 cout << "Enter the element: ";
-                cin >> q[rear + 1]; 
+                cin >> q[rear + 1];
             }
         } else {
             cout << "Queue is full." << endl;
         }
     }
+    void Insert() {
+        int item;
+        if (rear >= MAX - 1)
+            cout << "Queue is full...New elements cannot be inserted" << endl;
+        else {
+            cout << "Enter the item to insert: ";
+            cin >> item;
+            rear++;
+            q[rear] = item;
+            cout << "Element " << item << " inserted successfully!" << endl;
+            DisplayQueue();
+        }
+    }
     void DisplayQueue() {
-        if (front < rear) { 
+        if (front <= rear) {
             cout << "The Queue is: [";
-            for (; front <= rear; front++) { 
-                 cout << q[front];
-                 if (front < rear) {
+            for (int i=front; i <= rear; i++) {
+                cout << q[i];
+                if (i< rear) {
                     cout << ", ";
                 }
             }
-           cout << "]" << endl;
-       } else {
-           cout << "Queue is empty." << endl;
-       }
-   }
+            cout << "]" << endl;
+        } else {
+            cout << "Queue is empty." << endl;
+        }
+    }
 };
 
 int main() {
     int n;
+    char choice;
 top:
     cout << "Enter the Queue size: ";
     cin >> n;
@@ -53,5 +66,20 @@ top:
     Queue Q(n);
     Q.ReadQueue();
     Q.DisplayQueue();
+    do {
+        cout << "Do you want to insert an element? (y/n): ";
+        cin >> choice;
+        choice = toupper(choice);
+        switch (choice) {
+            case 'Y':
+                Q.Insert();
+                break;
+            case 'N':
+                cout << "Exiting...." << endl;
+                break;
+            default:
+                cout << "Invalid choice..Try again" << endl;
+        }
+    } while (choice != 'N');
     return 0;
 }
