bff7a68a1d4bbb67dcb669871e4ab4745a3a1df2
luffy-0012223 <abhinshelly@gmail.com>
2023-10-12 17:40:03 +0530

    added class

--- 5_BubbleSort.cpp
+++ 5_BubbleSort.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 #define MAX 5
 
-void Display(int a[],int& size){
+class Array{
+
+public:
+
+    void Display(int a[],int& size){
 
     cout<<endl;
     cout<<"Array : [ ";
@@ -15,23 +19,28 @@ void Display(int a[],int& size){
         }
     }
     cout<<" ]";
-}
+    }
 
-void BubbleSort(int a[],int& size){
+    void BubbleSort(int a[],int& size){
 
-    for(int i=0;i<size;i++){
-        for(int j=i+1;j<size;j++){
-            
-            if(a[j] < a[i]){
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                
+                if(a[j] < a[i]){
 
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
             }
         }
     }
-}
+};
+
 int main(){
+
+    Array A;
+
     int size;
     while(true){
         cout<<endl<<"Enter the size of the array : ";
@@ -48,11 +57,11 @@ int main(){
     }
 
     cout<<endl<<endl<<".....Array before Sort.......";
-    Display(a,size);
+    A.Display(a,size);
 
-    BubbleSort(a,size);
+    A.BubbleSort(a,size);
     cout<<endl<<endl<<".....Array after Sort.......";
-    Display(a,size);
+    A.Display(a,size);
 
     return 0;
 }
