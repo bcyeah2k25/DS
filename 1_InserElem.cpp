29a08c3dced0be286ccb82a2b269ff6b14e773ff
luffy-0012223 <abhinshelly@gmail.com>
2023-10-12 17:31:01 +0530

    added class

--- 1_InserElem.cpp
+++ 1_InserElem.cpp
@@ -1,94 +1,138 @@
 /*Write a program to insert an element anywhere using array,
 menu driven program*/
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 #define MAX 5
 
-void Display(int a[],int& size){
+class Array
+{
 
-    cout<<endl;
-    cout<<"Array : [ ";
-    for(int i=0;i<size;i++){
-        cout<<a[i];
-        if(i < size-1){
-            cout<<", ";
+public:
+
+    void Display(int a[], int &size)
+    {
+
+        cout << endl;
+        cout << "Array : [ ";
+        for (int i = 0; i < size; i++)
+        {
+            cout << a[i];
+            if (i < size - 1)
+            {
+                cout << ", ";
+            }
+        }
+        cout << " ]";
+    }
+
+    void Insert(int a[], int &size)
+    {
+
+        if (size == MAX)
+        {
+            cout << endl
+                 << "Insufficient space for insertion!";
+        }
+        else
+        {
+
+            int x, pos;
+
+            cout << "Enter the element to insert : ";
+            cin >> x;
+
+            while (true)
+            {
+                cout << endl
+                     << "Enter the position : ";
+                cin >> pos;
+                if (pos < 0 || pos > size)
+                {
+                    cout << endl
+                         << "Invalid position....Position should be from 0 to " << size << "!" << endl;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (int i = size; i > pos; i--)
+            {
+                a[i] = a[i - 1];
+            }
+            a[pos] = x;
+            size++;
+            cout << endl
+                 << "Element (" << x << ") succesfully inserted at position (" << pos << ")!";
         }
     }
-    cout<<" ]";
-}
+};
 
-void Insert(int a[],int& size){
-
-    if(size == MAX){
-        cout<<endl<<"Insufficient space for insertion!";
-    }else{
-
-        int x,pos;
-
-        cout<<"Enter the element to insert : ";
-        cin>>x;
-
-        while(true){    
-            cout<<endl<<"Enter the position : ";
-            cin>>pos;
-            if(pos < 0 || pos > size ){
-                cout<<endl<<"Invalid position....Position should be from 0 to "<<size<<"!"<<endl;
-            }else{break;}
-        }
-
-
-        for(int i=size;i>pos;i--){
-            a[i] = a[i-1];
-        }
-        a[pos] = x;
-        size++;
-        cout<<endl<<"Element ("<<x<<") succesfully inserted at position ("<<pos<<")!";
-    }
-}
-
-
-int main(){
-
+int main()
+{
+    Array A;
     int size;
-    while(true){
-        cout<<endl<<"Enter the size of the array : ";
-        cin>>size;
-        if(size > MAX || size < 1){
-            cout<<endl<<"Size should be between 1 and "<<MAX<<"!..Re-Try!";
-        }else{break;}
+    while (true)
+    {
+        cout << endl
+             << "Enter the size of the array : ";
+        cin >> size;
+        if (size > MAX || size < 1)
+        {
+            cout << endl
+                 << "Size should be between 1 and " << MAX << "!..Re-Try!";
+        }
+        else
+        {
+            break;
+        }
     }
 
     int a[MAX];
-    cout<<"Enter the elements of the array :-"<<endl;
-    for(int i=0;i<size;i++){
-        cin>>a[i];
+    cout << "Enter the elements of the array :-" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> a[i];
     }
 
     int choice;
 
-    do{
+    do
+    {
 
-        cout<<endl<<endl;
-        cout<<"1. Insert Element"<<endl
-            <<"2. Display Array"<<endl
-            <<"0. Exit"<<endl;
-        cout<<"Enter your choice : ";
-        cin>>choice;
+        cout << endl
+             << endl;
+        cout << "1. Insert Element" << endl
+             << "2. Display Array" << endl
+             << "0. Exit" << endl;
+        cout << "Enter your choice : ";
+        cin >> choice;
 
-        switch(choice){
+        switch (choice)
+        {
 
-            case 0 : cout<<endl<<endl<<"Exiting.........."<<endl; exit(0); break;
+        case 0:
+            cout << endl
+                 << endl
+                 << "Exiting.........." << endl;
+            exit(0);
+            break;
 
-            case 1 : Insert(a,size); break;
+        case 1:
+            A.Insert(a, size);
+            break;
 
-            case 2 : Display(a,size); break;
+        case 2:
+            A.Display(a, size);
+            break;
 
-            default : cout<<"Invalid choice....Re-Try!"<<endl;
-
+        default:
+            cout << "Invalid choice....Re-Try!" << endl;
         }
 
-    }while(choice != 0);
+    } while (choice != 0);
 
     return 0;
 }
