8aa6cf2ff5afc402d00c6ed76514c925bb1fcc23
luffy-0012223 <abhinshelly@gmail.com>
2023-10-15 11:09:24 +0530

    .

--- 29_DeleNodeMDLL.cpp
+++ 29_DeleNodeMDLL.cpp
@@ -147,30 +147,53 @@ public:
 
     void DelAftElem()
     {
-        int xi;
-        Node *node;
-        while (true)
+        if (isSingleElem())
         {
-            cout << "After which element to delete: ";
-            cin >> xi;
-            node = SearchNode(xi);
-            if (node != nullptr)
-            {
-                break;
-            }
-        }
-
-        if (node->next != nullptr)
-        {
-            Node *temp = node->next;
-            node->next = temp->next;
-            DispDeleted(temp->data);
-            delete temp;
-            DispLL();
+            return;
         }
         else
         {
-            cout << "No element to delete after " << xi << endl;
+            int xi;
+            Node *node;
+            while (true)
+            {
+                cout << "After which element to delete: ";
+                cin >> xi;
+                node = SearchNode(xi);
+                if (node != nullptr)
+                {
+                    break;
+                }
+            }
+
+            if (node->next != nullptr)
+            {
+                Node *temp = node->next;
+                node->next = temp->next;
+                DispDeleted(temp->data);
+                delete temp;
+                DispLL();
+            }
+            else
+            {
+                cout << "No element to delete after " << xi << endl;
+            }
+        }
+    }
+
+    bool isSingleElem()
+    {
+
+        if (start->next == nullptr)
+        {
+            cout << endl
+                 << endl
+                 << "Deletion after a element not possible single element left!" << endl;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 };
