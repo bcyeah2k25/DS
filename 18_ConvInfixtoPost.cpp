14772b68283c330bf037c971fad3418ec1492527
ab-h-i-n <abhinshelly@gmail.com>
2023-10-31 19:42:27 +0530

    updated

--- 18_ConvInfixtoPost.cpp
+++ 18_ConvInfixtoPost.cpp
@@ -1,180 +1,4 @@
-// /*Write a program to convert Infix to postfix expression / notation.*/
-
-// #include <iostream>
-// #include <string>
-// using namespace std;
-
-// class Stack
-// {
-//     char data[100];
-//     int top;
-
-// public:
-//     Stack()
-//     {
-//         top = -1;
-//     }
-
-//     void push(char c)
-//     {
-//         data[++top] = c;
-//     }
-
-//     char pop()
-//     {
-//         if (!isEmpty())
-//         {
-//             return data[top--];
-//         }
-//         return '\0';
-//     }
-
-//     char peek()
-//     {
-//         if (!isEmpty())
-//         {
-//             return data[top];
-//         }
-//         return '\0';
-//     }
-
-//     bool isEmpty()
-//     {
-//         return top == -1;
-//     }
-// };
-
-// class InfixToPostfix
-// {
-//     string infix;
-//     string postfix;
-
-//     int getPrecedence(char c)
-//     {
-//         if (c == '^')
-//             return 3;
-//         else if (c == '*' || c == '/')
-//             return 2;
-//         else if (c == '+' || c == '-')
-//             return 1;
-//         else
-//             return 0;
-//     }
-
-// public:
-//     void readInfix()
-//     {
-//         cout << "Enter an infix expression: ";
-//         getline(cin, infix);
-
-//         check();
-//     }
-
-//     void displayInfix()
-//     {
-//         cout << "Infix expression: " << infix << endl;
-//     }
-
-//     string convert()
-//     {
-//         Stack operatorStack;
-
-//         for (int i = 0; i < infix.length(); i++)
-//         {
-//             char c = infix[i];
-
-//             if (isalnum(c))
-//             {
-//                 string operand;
-//                 while (isalnum(infix[i]))
-//                 {
-//                     operand += infix[i];
-//                     i++;
-//                 }
-//                 i--;
-//                 postfix += operand + ' ';
-//             }
-//             else if (c == '(')
-//             {
-//                 operatorStack.push(c);
-//             }
-//             else if (c == ')')
-//             {
-//                 while (!operatorStack.isEmpty() && operatorStack.peek() != '(')
-//                 {
-//                     postfix += operatorStack.pop();
-//                     postfix += ' ';
-//                 }
-//                 if (!operatorStack.isEmpty() && operatorStack.peek() == '(')
-//                 {
-//                     operatorStack.pop();
-//                 }
-//             }
-//             else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
-//             {
-//                 while (!operatorStack.isEmpty() && getPrecedence(c) <= getPrecedence(operatorStack.peek()))
-//                 {
-//                     postfix += operatorStack.pop();
-//                     postfix += ' ';
-//                 }
-//                 operatorStack.push(c);
-//             }
-//         }
-
-//         while (!operatorStack.isEmpty())
-//         {
-//             postfix += operatorStack.pop();
-//             postfix += ' ';
-//         }
-
-//         return postfix;
-//     }
-
-//     bool isOperator(char c)
-//     {
-//         return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
-//     }
-
-//     void check()
-//     {
-
-//         int operand = 0, operatorr = 0;
-
-//         for (char c : infix)
-//         {
-//             if (isdigit(c))
-//             {
-//                 operand++;
-//             }
-//             else if (isOperator(c))
-//             {
-//                 operatorr++;
-//             }
-//         }
-
-//         if (operatorr != operand - 1 || isOperator(infix[infix.length() - 1]))
-//         {
-//             cout << endl
-//                  << endl
-//                  << "Invalid expression!..Re-Enter..!" << endl
-//                  << endl;
-//             readInfix();
-//         }
-//     }
-// };
-
-// int main()
-// {
-//     InfixToPostfix converter;
-//     converter.readInfix();
-//     converter.displayInfix();
-//     string postfixExpression = converter.convert();
-
-//     cout << "Postfix expression: " << postfixExpression << endl;
-
-//     return 0;
-// }
-
+// (not corrected)write a program to convert infix expression to postfix
 #include <iostream>
 #include <string>
 using namespace std;
@@ -185,9 +9,15 @@ class Stack
     int top;
 
 public:
-    Stack() { top = -1; }
+    Stack()
+    {
+        top = -1;
+    }
 
-    void push(char c) { data[++top] = c; }
+    void push(char c)
+    {
+        data[++top] = c;
+    }
 
     char pop()
     {
@@ -207,12 +37,16 @@ public:
         return '\0';
     }
 
-    bool isEmpty() { return top == -1; }
+    bool isEmpty()
+    {
+        return top == -1;
+    }
 };
 
 class InfixToPostfix
 {
-    string infix, postfix;
+    string infix;
+    string postfix;
 
     int getPrecedence(char c)
     {
@@ -226,96 +60,21 @@ class InfixToPostfix
             return 0;
     }
 
-    bool isOperand(char c)
-    {
-        return (isalpha(c) || isdigit(c));
-    }
-
-    bool isOperator(char c)
-    {
-        return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
-    }
-
-    bool isOpenPar(char c)
-    {
-        return (c == '(');
-    }
-
-    bool isClosedPar(char c)
-    {
-        return (c == ')');
-    }
-
-    void validate()
-    {
-        int opCount = 0, opndCount = 0, parCount = 0;
-        bool prevCharOperator = true; // Initially set to true to handle cases like '-5'.
-
-        for (char c : infix)
-        {
-            if (isOperand(c))
-            {
-                opndCount++;
-                prevCharOperator = false;
-            }
-            else if (isOperator(c))
-            {
-                opCount++;
-                if (prevCharOperator && (c != '-' && c != '+'))
-                {
-                    cout << "Invalid consecutive operators" << endl;
-                    return;
-                }
-                prevCharOperator = true;
-            }
-            else if (isOpenPar(c))
-            {
-                parCount++;
-                prevCharOperator = true;
-            }
-            else if (isClosedPar(c))
-            {
-                parCount--;
-                prevCharOperator = false;
-            }
-        }
-
-        if (parCount != 0)
-        {
-            cout << "Unbalanced parentheses" << endl;
-            return;
-        }
-
-        if (opCount != opndCount - 1)
-        {
-            cout << "Invalid operator/operand counts" << endl;
-            return;
-        }
-
-        if (!isOperand(infix[0]) || !isOperand(infix[infix.length() - 1]))
-        {
-            cout << "Invalid start/end characters" << endl;
-            return;
-        }
-
-        if (infix.length() == 0)
-        {
-            cout << "Empty expression" << endl;
-            return;
-        }
-
-        cout << "Valid expression" << endl;
-    }
-
 public:
     void readInfix()
     {
-        cout << "Enter infix expression: ";
+        cout << "Enter an infix expression: ";
         getline(cin, infix);
-        validate();
+
+        check();
     }
 
-    string convertToPostfix()
+    void displayInfix()
+    {
+        cout << "Infix expression: " << infix << endl;
+    }
+
+    string convert()
     {
         Stack operatorStack;
 
@@ -369,14 +128,75 @@ public:
 
         return postfix;
     }
+
+    bool isOperator(char c)
+    {
+        return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+    }
+
+    void check()
+    {
+        char ch;
+
+        for (int i = 0; i < infix.length() - 1;)
+        {
+            for (int j = i + 1; j < infix.length(); j++)
+            {
+                while (infix[j] == ' ')
+                {
+                    ++j;
+                }
+                if ((isalnum(infix[i])) && (isalnum(infix[j])))
+                {
+                    cout << endl
+                         << endl
+                         << "Invalid expression!..Re-Enter..!" << endl
+                         << endl;
+
+                    readInfix();
+                }
+                else if ((isOperator(infix[i])) && (isOperator(infix[j])))
+                {
+                    cout << endl
+                         << endl
+                         << "Invalid expression!..Re-Enter..!" << endl
+                         << endl;
+
+                    readInfix();
+                }
+                i++;
+                while (infix[i] == ' ')
+                {
+                    i++;
+                    j++;
+                }
+            }
+        }
+
+        if (infix[0] == '(')
+            ch = infix[1];
+        else
+            ch = infix[0];
+        if (isOperator(infix[infix.length() - 1]) || isOperator(ch))
+        {
+
+            cout << endl
+                 << endl
+                 << "Invalid expression!..Re-Enter..!" << endl
+                 << endl;
+            readInfix();
+        }
+    }
 };
 
 int main()
 {
-    InfixToPostfix i;
-    i.readInfix();
-    string postfix = i.convertToPostfix();
-    cout << "Postfix expression: " << postfix << endl;
+    InfixToPostfix converter;
+    converter.readInfix();
+    converter.displayInfix();
+    string postfixExpression = converter.convert();
+
+    cout << "Postfix expression: " << postfixExpression << endl;
 
     return 0;
 }
