7a14e1d28e3f6eb7256204752a0e1dbcc1399b42
ab-h-i-n <abhinshelly@gmail.com>
2023-10-31 20:32:00 +0530

    .

--- 35_CircularLL.cpp
+++ 35_CircularLL.cpp
@@ -1,8 +1,6 @@
 /*Write a C++ program to create and
 insert an element anywhere in the circular list.*/
 
-// not complete
-
 #include <iostream>
 using namespace std;
 
@@ -22,6 +20,7 @@ public:
     CircularLinkedList()
     {
         start = nullptr;
+        n = 0;
     }
 
     void DispLL()
@@ -48,9 +47,8 @@ public:
     void CreateLL()
     {
         int x;
-        n++;
         cout << endl
-             << "Enter element " << n << " : ";
+             << "Enter element " << (n + 1) << " : ";
         cin >> x;
         Insert(x);
 
@@ -84,13 +82,94 @@ public:
             curr->next = temp;
             temp->next = start;
         }
+        n++;
+    }
+
+    Node *SearchNode(int pos)
+    {
+        Node *curr = start;
+        for (int i = 0; i < pos; i++)
+        {
+            curr = curr->next;
+        }
+        return curr;
+    }
+
+    void InsertAtPos()
+    {
+
+        int pos, x;
+        cout << endl
+             << "Enter the element to insert : ";
+        cin >> x;
+        while (true)
+        {
+            cout << endl
+                 << "Enter the position you want to enter : ";
+            cin >> pos;
+            pos--;
+            if (pos < 0 || pos > n)
+            {
+                cout << endl
+                     << endl
+                     << "Invalid position...Re-Enter!" << endl;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (pos == 0)
+        {
+            Node *newNode = new Node;
+            Node *temp = start;
+
+            while (temp->next != start)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = newNode;
+            newNode->next = start;
+            start = newNode;
+            newNode->data = x;
+        }
+        else
+        {
+            Node *newNode = new Node;
+            Node *prevNode = SearchNode(pos - 1);
+
+            newNode->data = x;
+            newNode->next = prevNode->next;
+            prevNode->next = newNode;
+        }
+        n++;
+        cout << endl
+             << endl
+             << "Element (" << x << ") has been inserted at position (" << (pos + 1) << ")" << endl;
+
+        DispLL();
     }
 };
 
 int main()
 {
+    char ch;
     CircularLinkedList list;
     list.CreateLL();
     list.DispLL();
+    do
+    {
+        list.InsertAtPos();
+        cout << endl
+             << endl
+             << "Insert another element ? ('y' to continue) : ";
+        cin >> ch;
+        if (ch != 'y')
+        {
+            break;
+        }
+    } while (true);
     return 0;
 }
